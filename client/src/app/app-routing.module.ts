7bb2367862b5d604e660a81b9decc015b1357651
Elena <lam00071@morris.umn.edu>
2021-02-24 17:53:32 -0600

    Add path for todos in App Routing Module.ts
    Import ToDoProfileComponent and ToDosListComponent and add path for todo main page and specified to do with id.
    Co-authored: @lam00071 @jadynsondrol

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ToDoProfileComponent } from './todos/todo-profile.component';
+import { ToDosListComponent } from './todos/todos.list.component';
 import { UserListComponent } from './users/user-list.component';
 import { UserProfileComponent } from './users/user-profile.component';
 
@@ -8,7 +10,9 @@ import { UserProfileComponent } from './users/user-profile.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UserListComponent},
-  {path: 'users/:id', component: UserProfileComponent}
+  {path: 'users/:id', component: UserProfileComponent},
+  {path: 'todos', component: ToDosListComponent},
+  {path: 'todos/:id', component: ToDoProfileComponent}
 ];
 
 @NgModule({
